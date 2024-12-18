f0cdb9a0e5af93c4de80f1d5d21b02acfa4e5c9f
ateli9r <ateli9r@gmail.com>
2024-12-18 21:41:42 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // resources/templates/home.html 을 렌더링
+    }
+}
